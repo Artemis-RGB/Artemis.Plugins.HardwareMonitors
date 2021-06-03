6d0b901323cdadc0a8a50a6544424c784930a6a7
Diogo Trindade <diogotr7@gmail.com>
2021-06-03 16:03:58 +0100

    initial commit

---
+++ src/Artemis.Plugins.HardwareMonitors.Aida64/AidaElement.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml;
+
+namespace Artemis.Plugins.HardwareMonitors.Aida64
+{
+    internal class AidaElement
+    {
+        public string Type { get; set; }
+        public string Id { get; set; }
+        public string Label { get; set; }
+        public string Value { get; set; }
+
+        public static AidaElement FromXmlElement(XmlElement item)
+        {
+            return new AidaElement
+            {
+                Type = item.Name,
+                Id = item["id"].InnerText,
+                Label = item["label"].InnerText,
+                Value = item["value"].InnerText
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{Type} - {Label} - {Value}";
+        }
+    }
+}
